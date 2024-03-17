def13b7cfa5aa0084b799e5ec18b95692e153302
muhammedbayhan <muhammedbayhan72@gmail.com>
2024-03-17 23:57:32 +0300

    ex6 appbar

--- lib/example/example6.dart
+++ lib/example/example6.dart
@@ -12,15 +12,37 @@ class _Example6State extends State<Example6> {
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: ExampleColors.ex6Bg,
-      appBar: AppBar(backgroundColor: ExampleColors.ex6Bg,),
+      appBar: AppBar(backgroundColor: ExampleColors.ex6Bg,
+      leading: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleIcon(),
+        ],
+      ),
+      title: Text("EXAMPLE 6",style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: ExampleColors.ex6White,fontWeight: FontWeight.bold),),
+      centerTitle: true,
+      actions: [CircleIcon(),],
+      ),
       body: Column(children: [
-
+Card(child: Container(color: ExampleColors.ex6Bg.withOpacity(0.9),),)
       ],),
     );
+  }
+}
+
+class CircleIcon extends StatelessWidget {
+  const CircleIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(child: Icon(Icons.grid_view_rounded));
   }
 }
 
 
 class ExampleColors{
   static Color ex6Bg=Color.fromRGBO(14, 48, 62, 1);
+  static Color ex6White=Color.fromRGBO(255, 255, 255, 1);
 }
